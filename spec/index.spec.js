0840f05804519ccc23ab24762f25b4cb2b8a9472
Timotheus Kampik <timotheus.kampik@signavio.com>
2017-10-15 13:44:18 +0200

    #2 further refine test

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -6,6 +6,7 @@ describe('translateGettext()', () => {
     let nonFuzzyEntry
     let fuzzyEntryComment
     let fuzzyEntry
+    let entryFuzzyAfterOtherComment
 
     const setupTranslations = (input, mode, callback) => {
         translateGettext(input, mode, processedPoFile => {
@@ -14,6 +15,7 @@ describe('translateGettext()', () => {
             nonFuzzyEntry = processedTranslations['Control']
             fuzzyEntryComment = processedTranslations['fuzzy test comment']
             fuzzyEntry = processedTranslations['Copy all contents']
+            entryFuzzyAfterOtherComment = processedTranslations['fuzzy suceeds other comment']
             callback()
         })
     }
@@ -77,6 +79,10 @@ describe('translateGettext()', () => {
         })
         it('should not newly translate non-empty entries', () => {
             expect(nonFuzzyEntry.msgstr[0]).toEqual('DONT_TRANSLATE_AGAIN')
+        })
+        it('should handle fuzzy entries with futher comments correctly', () => {
+            expect(fuzzyEntryComment.comments.flag).not.toEqual('fuzzy')
+            expect(entryFuzzyAfterOtherComment.comments.flag).not.toEqual('fuzzy')
         })
     })
 
