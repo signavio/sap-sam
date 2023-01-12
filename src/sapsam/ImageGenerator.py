04fee87082fb7a114b2b2b9d96c331732819a71d
Timotheus Kampik <timotheus.kampik@signavio.com>
2023-01-12 15:20:15 +0100

    Auto-delete diagrams that have been create for PNG/XML generation

--- src/sapsam/ImageGenerator.py
+++ src/sapsam/ImageGenerator.py
@@ -7,6 +7,18 @@ class ImageGenerator:
     Class that generates images based on JSON or XML representations
     """
 
+    def _delete_diagram(self, id: str):
+        """Deletes a diagram in a SAP Signavio Process Manager workspace (by ID)
+
+        Args:
+            id (str): diagram/model ID
+        """
+        auth_data = SignavioAuthenticator.authenticate()
+        model_url = system_instance + '/p/model'
+        cookies = {'JSESSIONID': auth_data['jsesssion_ID'], 'LBROUTEID': auth_data['lb_route_ID']}
+        headers = {'Accept': 'application/json', 'x-signavio-id':  auth_data['auth_token']}
+        requests.delete(f'{model_url}/{id}', cookies=cookies, headers=headers)
+
     def _setup_folder(self):
         """Creates a folder named 'SAP-SAM' in the 'Shared Documents' directory
         of the workspace, if a folder with such a name does not exist
@@ -43,7 +55,7 @@ class ImageGenerator:
             data={'name': 'SAP-SAM', 'parent': f'/directory/{shared_docs_id}'})
             return json.loads(create_dir_request.content)['href'].replace('/directory/', '')
 
-    def generate_representation(self, name, data, namespace, rep):
+    def generate_representation(self, name, data, namespace, rep, deletes=True):
         """Uploads a diagram to the SAP-SAM folder in Signavio Process Manager
         and returns a diagram representation, e.g., as PNG or XML.
 
@@ -52,6 +64,8 @@ class ImageGenerator:
             data (str): JSON representation of the diagram
             namespace (str): Namespace of the diagram
             rep (str): The representation that should be returned: 'json', 'bpmn2_0_xml', 'png', or 'svg'
+            deletes (bool): If True, deletes diagram after content has been generated and returned.
+                            Default: True
 
 
 
@@ -74,15 +88,18 @@ class ImageGenerator:
             headers=headers,
             data=data)
         result = json.loads(create_diagram_request.content)
+        model_id = result['href'].replace('/model/', '')
         revision_id = result['rep']['revision'].replace('/revision/', '')
         diagram_url = system_instance + '/p/revision'
         rep_request = requests.get(
             f'{diagram_url}/{revision_id}/{rep}',
             cookies=cookies,
             headers=headers)
+        if deletes:
+            self._delete_diagram(model_id)
         return rep_request.content
 
-    def generate_image(self, name, data, namespace):
+    def generate_image(self, name, data, namespace, deletes=True):
         """Uploads a diagram to the SAP-SAM folder in Signavio Process Manager
         and returns a diagram representation as PNG.
 
@@ -90,15 +107,16 @@ class ImageGenerator:
             name (str): Name of the diagram
             data (str): JSON representation of the diagram
             namespace (str): Namespace of the diagram
-
+            deletes (bool): If True, deletes diagram after content has
+                            been generated and returned. Default: True
 
 
         Returns:
             PNG representation of the diagram
         """
-        return self.generate_representation(name, data, namespace, 'png')
+        return self.generate_representation(name, data, namespace, 'png', deletes)
     
-    def generate_xml(self, name, data, namespace):
+    def generate_xml(self, name, data, namespace, deletes=True):
         """Uploads a diagram to the SAP-SAM folder in Signavio Process Manager
         and returns a diagram representation as BPMN 2.x XML.
 
@@ -106,12 +124,12 @@ class ImageGenerator:
             name (str): Name of the diagram
             data (str): JSON representation of the diagram
             namespace (str): Namespace of the diagram
-
-
+            deletes (bool): If True, deletes diagram after content has
+                been generated and returned. Default: True
 
         Returns:
             BPMN 2.x XML representation of the diagram
         """
-        return self.generate_representation(name, data, namespace, 'bpmn2_0_xml')
+        return self.generate_representation(name, data, namespace, 'bpmn2_0_xml', deletes)
 
     
